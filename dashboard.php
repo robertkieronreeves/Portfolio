4fb109d5edb9110c423778d0937612671ac4cc35
Rob <robertkieronreeves@gmail.com>
2018-10-01 11:07:36 +0100

    added html for the forms and dashboard

--- dashboard.php
+++ dashboard.php
@@ -11,23 +11,54 @@
 </head>
 <body>
     <h1>DASHBOARD PAGE</h1>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        <h2>Intro:</h2>
-            Title text:
-                <input name="title_text" input type="text"><br>
-            Intro text:
-                <input name="intro_text" input type="text"><br>
-        <input type="submit" name="Submit All" value="submit">
-    </form>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-
-    <h2>About me:</h2>
-            Bio paragraph:
-                <input name="bio_paragraph" input type="text"><br>
-            Image:
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" name="Submit All" value="submit">
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <h2>Intro:</h2>
+                Title text:
+                    <input name="title_text" input type="text"><br>
+                Intro text:
+                    <input name="intro_text" input type="text"><br>
+                    <input type="submit" name="Submit All" value="submit">
+        </form>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <h2>About me:</h2>
+                Bio paragraph 1:
+                    <input name="bio_paragraph" input type="text"><br>
+                Image:
+                    <input type="file" name="fileToUpload" id="fileToUpload"><br>
+                Bio paragraph 2:
+                    <input name="bio_paragraph_2" input type="text"><br>
+                    <input type="submit" name="Submit All" value="submit">
+        </form>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <h2>Projects:</h2>
+                Project 1 Title:
+                    <input name="project_title" input type="text"><br>
+                Image:
+                    <input type="file" name="fileToUpload" id="fileToUpload"><br>
+                Project 1 Paragraph:
+                    <input name="project_paragraph" input type="text"><br>
+                    <input type="submit" name="Submit All" value="submit"><br>
+                Project 2 Title:
+                    <input name="project_title" input type="text"><br>
+                Image:
+                    <input type="file" name="fileToUpload" id="fileToUpload"><br>
+                Project 2 Paragraph:
+                    <input name="project_paragraph" input type="text"><br>
+                    <input type="submit" name="Submit All" value="submit"><br>
+                Project 3 Title:
+                    <input name="project_title" input type="text"><br>
+                Image:
+                    <input type="file" name="fileToUpload" id="fileToUpload"><br>
+                Project 3 Paragraph:
+                    <input name="project_paragraph" input type="text"><br>
+                    <input type="submit" name="Submit All" value="submit"><br>
+                Project 4 Title:
+                    <input name="project_title" input type="text"><br>
+                Image:
+                    <input type="file" name="fileToUpload" id="fileToUpload"><br>
+                Project 4 Paragraph:
+                    <input name="project_paragraph" input type="text"><br>
+                    <input type="submit" name="Submit All" value="submit"><br>
+        </form>
 </body>
 </html>
