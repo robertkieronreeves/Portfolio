019944955a52c38cc4adc4bcd3faa56064e0057e
Rob <robertkieronreeves@gmail.com>
2018-10-02 16:25:20 +0100

    tidied up code

--- index.php
+++ index.php
@@ -4,12 +4,12 @@ require 'functions.php';
 
 $db = displayPDO();
 
-$stmt = $db->query("SELECT `title` ,`intro_text` FROM `intro`");
-$title_array = $stmt->fetch();
-$stmt = $db->query("SELECT `paragraph_1`, `image`, `paragraph_2` FROM `about_me`");
-$about_me_array = $stmt->fetch();
-$stmt = $db->query("SELECT `title`, `image`, `paragraph` FROM `projects`");
-$projects_array = $stmt->fetch();
+    $stmt = $db->query("SELECT `title` ,`intro_text` FROM `intro`");
+    $title_array = $stmt->fetch();
+    $stmt = $db->query("SELECT `paragraph_1`, `image`, `paragraph_2` FROM `about_me`");
+    $about_me_array = $stmt->fetch();
+    $stmt = $db->query("SELECT `title`, `image`, `paragraph` FROM `projects`");
+    $projects_array = $stmt->fetch();
 
 $data = array_merge($title_array, $about_me_array, $projects_array);
 ?>
